90086cd2362f86485115a5eb2790cb8baeb58413
Dr Maxis <axelrousseau935@gmail.com>
2021-10-13 18:12:24 +0200

    site CovidInfo prototype

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("actu").onclick = function () {
+  location.href = "index.html";
+};
+document.getElementById("campagne").onclick = function () {
+  location.href = "campagne.html";
+};
+document.getElementById("solidaire").onclick = function () {
+  location.href = "solidaire.html";
+};
